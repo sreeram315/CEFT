0c5651d0ce2f0215b55ede7e9c39f62e0c7fce6c
sreeram315 <sreerammaram2@gmail.com>
2021-12-09 12:53:48 +0530

    updated util.py

--- UI_SOURCE/CEFT/utils/utils.py
+++ UI_SOURCE/CEFT/utils/utils.py
@@ -1,4 +1,8 @@
-def cropImage(imagePath, aspectRatio, salientCoordinates, outputFileName = "output"):
+def isInsideCrop(x, y, crop):
+	left, right, top, bottom = crop[0], crop[1], crop[2], crop[3]
+	return (x >= left and x <= right and y>=top and y<=bottom)
+
+def cropImage(imagePath, aspectRatio, salientCoordinates, top_feature, outputFileName = "output"):
 	from PIL import Image
 	originalImage = Image.open(imagePath)
 	width, height = originalImage.size
@@ -23,7 +27,30 @@ def cropImage(imagePath, aspectRatio, salientCoordinates, outputFileName = "outp
 		left 		= max(0, salientCoordinates[0] - (finalWidth/2))
 		right 		= left + finalWidth
 
-	# print(top, bottom, left, right)
+
+	m1_x, m1_y = top_feature[0], max(0, top_feature[1]-(0.05 * height))
+	m2_x, m2_y = top_feature[0], min(top_feature[1]+(0.05 * height), height)
+	m3_x, m3_y = max(0, top_feature[0]-(0.05 * width)), top_feature[1]
+	m4_x, m4_y = min(top_feature[0]+(0.05 * width), width), top_feature[1]
+
+	# print("-> ", left, right, top, bottom)
+	crop = (left, right, top, bottom)
+	if not all([isInsideCrop(m1_x, m1_y, crop), isInsideCrop(m2_x, m2_y, crop), isInsideCrop(m3_x, m3_y, crop), isInsideCrop(m4_x, m4_y, crop)]):
+		print(f"Going for top feature for {outputFileName}")
+		cropImage(imagePath, aspectRatio, top_feature, top_feature, outputFileName)
+		return
 	croppedImage = originalImage.crop((left, top, right, bottom))
 	croppedImage.show()
 	croppedImage.save(f"{outputFileName}.jpeg")
+
+
+
+
+
+
+
+
+
+
+
+
